485614da7d9e59e40a68f7540aadda62413d516c
kesihinz <keishin.chou@gmail.com>
2019-11-02 09:55:08 +0900

    Initial Commit

---
+++ Swifty7Words/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swifty7Words
+//
+//  Created by Keishin CHOU on 2019/11/02.
+//  Copyright © 2019 Keishin CHOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
